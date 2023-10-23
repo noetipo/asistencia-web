cb65cc50ea6c2aaae3b432cb31aad6169d76f00b
pacifi <pacifi.bnr@gmail.com>
2023-10-22 21:51:21 -0500

    docker start

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: "http://localhost:8080/",
+  url: "https://asistenciamonolitoback.imperiumse.com/",
 
   //url: "http://127.0.0.1:8000/api/auth/",
 };
